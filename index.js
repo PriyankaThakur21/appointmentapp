5f9038fc4efe9a8c5ae7125a8bfeb7a9e861a841
PriyankaThakur21 <thakur.pri112@gmail.com>
2023-05-07 15:55:04 +0530

    firstcommit

---
+++ index.js
@@ -0,0 +1,17 @@
+function save(event){
+    event.preventDefault();
+    const name=event.target.name.value;
+    const email=event.target.email.value;
+    const phone=event.target.phn.value;
+    const obj={name,email,phone};
+    axios.post('https://crudcrud.com/api/6ba832c5062341ccb09ae2b32fbc6c28/appointmentapp',obj)
+    .then((resolve)=>showonscreen(resolve.data))
+    .catch((err)=>console.log(err))
+    event.target.reset();
+    }
+    function showonscreen(obj){
+        const p=document.body;
+        const list=document.createElement('li');
+        list.textContent=list.textContent+obj.name+'--'+obj.email+'--'+obj.phone;
+        p.append(list);
+    }
