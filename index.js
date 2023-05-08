6c77ae5b6c2ffb5f35ec57709f720af29b2d5b92
PriyankaThakur21 <thakur.pri112@gmail.com>
2023-05-08 17:42:41 +0530

    newcommit3

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ function save(event){
     const email=event.target.email.value;
     const phone=event.target.phn.value;
     const obj={name,email,phone};
-    axios.post('https://crudcrud.com/api/6ba832c5062341ccb09ae2b32fbc6c28/appointmentapp',obj)
+    axios.post('https://crudcrud.com/api/446f33725d21403da80c25570a98995c/appointmentapp',obj)
     .then((resolve)=>showonscreen(resolve.data))
     .catch((err)=>console.log(err))
     event.target.reset();
     }
     window.addEventListener('DOMContentLoaded', ()=>{
-        axios.get('https://crudcrud.com/api/6ba832c5062341ccb09ae2b32fbc6c28/appointmentapp')
+        axios.get('https://crudcrud.com/api/446f33725d21403da80c25570a98995c/appointmentapp')
         .then((resolve)=>{
         for(let i in resolve.data){
             showonscreen(resolve.data[i]);
@@ -20,8 +20,29 @@ function save(event){
         })
     })
     function showonscreen(obj){
-        const p=document.body;
+        const p=document.getElementById('block1');
         const list=document.createElement('li');
+        list.id=obj._id
         list.textContent=list.textContent+obj.name+'--'+obj.email+'--'+obj.phone;
-        p.append(list);
+        const deletebtn=document.createElement('input')
+        deletebtn.type='button'
+        deletebtn.value='delete'
+        deletebtn.onclick=function() {deletedata(obj._id)};
+        list.appendChild(deletebtn)
+        p.appendChild(list);
+    }
+    function deletedata(userid){
+        axios.delete(`https://crudcrud.com/api/446f33725d21403da80c25570a98995c/appointmentapp/${userid}`)
+        .then((resolve)=>{
+            removeuserfromscreen(userid)
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+    }
+    function removeuserfromscreen(userid){
+        const parentel=document.getElementById('block1');
+        const childel=document.getElementById(userid)
+        console.log('hi')
+            parentel.removeChild(childel)
     }
