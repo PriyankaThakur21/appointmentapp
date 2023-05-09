3d875109d65e8b2e12a8da4759a41bcaaffed3b5
PriyankaThakur21 <thakur.pri112@gmail.com>
2023-05-09 18:03:48 +0530

    finalcommit

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ function save(event){
     const email=event.target.email.value;
     const phone=event.target.phn.value;
     const obj={name,email,phone};
-    axios.post('https://crudcrud.com/api/446f33725d21403da80c25570a98995c/appointmentapp',obj)
+    axios.post('https://crudcrud.com/api/9ba78636c85d4cdb862d92054128522b/appointmentapp',obj)
     .then((resolve)=>showonscreen(resolve.data))
     .catch((err)=>console.log(err))
     event.target.reset();
     }
     window.addEventListener('DOMContentLoaded', ()=>{
-        axios.get('https://crudcrud.com/api/446f33725d21403da80c25570a98995c/appointmentapp')
+        axios.get('https://crudcrud.com/api/9ba78636c85d4cdb862d92054128522b/appointmentapp')
         .then((resolve)=>{
         for(let i in resolve.data){
             showonscreen(resolve.data[i]);
@@ -27,12 +27,17 @@ function save(event){
         const deletebtn=document.createElement('input')
         deletebtn.type='button'
         deletebtn.value='delete'
+        const editbtn=document.createElement('input')
+        editbtn.type='button'
+        editbtn.value='edit'
+        editbtn.onclick=function() {editdata(obj._id)};
         deletebtn.onclick=function() {deletedata(obj._id)};
         list.appendChild(deletebtn)
+        list.appendChild(editbtn)
         p.appendChild(list);
     }
     function deletedata(userid){
-        axios.delete(`https://crudcrud.com/api/446f33725d21403da80c25570a98995c/appointmentapp/${userid}`)
+        axios.delete(`https://crudcrud.com/api/9ba78636c85d4cdb862d92054128522b/appointmentapp/${userid}`)
         .then((resolve)=>{
             removeuserfromscreen(userid)
         })
@@ -45,4 +50,17 @@ function save(event){
         const childel=document.getElementById(userid)
         console.log('hi')
             parentel.removeChild(childel)
+    }
+    function editdata(userid){
+        axios.get(`https://crudcrud.com/api/9ba78636c85d4cdb862d92054128522b/appointmentapp/${userid}`)
+        .then((resolve)=>{
+            this.obj=resolve.data
+            edituserdetail(obj.name, obj.email, obj.phone,userid)
+        })
+    }
+    function edituserdetail(newname,newemail,newphn,userId){
+        document.getElementById('name').value=newname
+        document.getElementById('email').value=newemail
+        document.getElementById('phn').value=newphn
+        deletedata(userId)
     }
